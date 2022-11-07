201e53893eef3081e8df7f907bf0be2145c2b744
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-11-07 12:07:31 +0530

    New Commit

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -175,7 +175,8 @@ def main(args):
 
                 print("type of user_permissions ::", type(user_permissions))
                 for i in user_permissions:
-                    print(i.__dict__)
+                    print(type(i))
+                    print(i)
 
                 # Step: Update Project permissions
                 # add_permission(data, wb_id[0], user_id, version, auth_token)

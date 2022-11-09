555cd2fe4f2bbb7c151225d61773d8a521f536e5
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-11-09 16:49:51 +0530

    New Commit

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -198,6 +198,9 @@ def main(arguments):
                             # Step: Get the Workbook ID from the Workbook Name
                             wb_id = get_workbook_id(server, data)[0]
 
+                            print(f"permission_data['permission_group_name'] :: {permission_data['permission_group_name']}")
+                            print(f"permission_data['permission_user_name'] :: {permission_data['permission_user_name']}")
+                            
                             # Step: Get the User or Group ID of permission assigned
                             if permission_data['permission_group_name'] is not None and permission_data['permission_user_name'] is None:
                                 permission_user_or_group_id = get_group_id(
